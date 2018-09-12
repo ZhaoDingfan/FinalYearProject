1d6370577651c7b07765f16947f22029e2682e7a
dingfan <zhaodingfanhaha@gmail.com>
2018-09-12 20:47:28 +0800

    Initial commit

---
+++ VAE impl/Basic_VAE.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+
+from torch import nn
+
+device = torch.device("cpu")
+# Once GPU is available it will use GPU to train the model
+# device = torch.device("cuda")
+
+
+
+
